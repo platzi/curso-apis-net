5cd1decf5df43f80ada0003490d346a22699455b
Miguel Teheran <miguelteheran@gmail.com>
2022-03-17 18:50:11 -0500

    agregando controladores

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+using webapi.Services;
+
+namespace webapi.Controllers;
+
+[Route("api/[controller]")]
+public class CategoriaController: ControllerBase
+{
+    ICategoriaService categoriaService;
+
+    public CategoriaController(ICategoriaService service)
+    {
+        categoriaService = service;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(categoriaService.Get());
+    }
+
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Categoria categoria)
+    {
+        categoriaService.Save(categoria);
+        return Ok();
+    }
+
+
+    [HttpPut("{id}")]
+    public IActionResult Put(Guid id, [FromBody] Categoria categoria)
+    {
+        categoriaService.Update(id, categoria);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        categoriaService.Delete(id);
+        return Ok();
+    }    
+
+}
---
+++ Controllers/TareaController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+using webapi.Services;
+
+namespace webapi.Controllers;
+
+[Route("api/[controller]")]
+public class TareaController : ControllerBase
+{
+    ITareasService tareasService;
+
+    public TareaController(ITareasService service)
+    {
+        tareasService = service;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(tareasService.Get());
+    }
+}
