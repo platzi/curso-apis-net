41391d751aa9c78aadcd57fb2d2ae5672e2e0596
Miguel Teheran <miguelteheran@gmail.com>
2022-03-17 19:18:58 -0500

    conetando a BD

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,14 +8,26 @@ public class HelloWorldController:  ControllerBase
 {
     IHelloWorldService helloWorldService;
 
-    public HelloWorldController(IHelloWorldService helloWorld)
+    TareasContext dbcontext;
+
+    public HelloWorldController(IHelloWorldService helloWorld, TareasContext db)
     {
         helloWorldService = helloWorld;
+        dbcontext = db;
     }
 
     [HttpGet]
     public IActionResult Get()
     {
         return Ok(helloWorldService.GetHelloWorld());
+    }
+
+    [HttpGet]
+    [Route("createdb")]
+    public IActionResult CreateDatabase()
+    {
+        dbcontext.Database.EnsureCreated();
+
+        return Ok();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using webapi;
 using webapi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<TareasContext>("Data Source=server;Initial Catalog=TareasDb;user id=sa;password=pass");
 //builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
 builder.Services.AddScoped<IHelloWorldService>(p=> new HelloWorldService());
 builder.Services.AddScoped<ICategoriaService, CategoriaService>();
